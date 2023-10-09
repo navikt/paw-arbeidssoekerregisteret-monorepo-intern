b014a9ad3c6439b79c5bce4b5b15af97baabc498
Nils Martin Sande <nils.martin.sande@nav.no>
2023-10-09 15:11:08 +0200

    Oppdaterte unit-test for KafkaStream

--- app/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/app/ApplikasjonsTest.kt
+++ app/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/app/ApplikasjonsTest.kt
@@ -1,30 +1,39 @@
 package no.nav.paw.arbeidssokerregisteret.app
 
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import no.nav.paw.arbeidssokerregisteret.intern.v1.Hendelse
 import no.nav.paw.arbeidssokerregisteret.intern.v1.Start
+import org.apache.avro.specific.SpecificRecord
+import org.apache.kafka.common.serialization.Deserializer
+import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.serialization.Serializer
 import org.apache.kafka.common.utils.Time
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.TopologyTestDriver
 import org.apache.kafka.streams.state.internals.InMemoryKeyValueBytesStoreSupplier
 import org.apache.kafka.streams.state.internals.KeyValueStoreBuilder
+import java.nio.ByteBuffer
 import java.time.Instant
 import java.util.*
 
+val SCHEMA_REGISTRY_SCOPE = ApplikasjonsTest::class.java.getName();
 
-class ApplikasjonsTest: StringSpec({
+
+class ApplikasjonsTest : StringSpec({
     "test av applikasjon" {
+        val hendelseSerde: Serde<Hendelse> = GenericSerde()
+        val periodeSerde: Serde<PeriodeTilstandV1> = GenericSerde()
+
         val builder = StreamsBuilder()
         builder.addStateStore(
             KeyValueStoreBuilder(
                 InMemoryKeyValueBytesStoreSupplier("tilstandsDb"),
                 Serdes.String(),
-                SpecificAvroSerde<PeriodeTilstandV1>(),
+                periodeSerde,
                 Time.SYSTEM
             )
         )
@@ -38,18 +47,20 @@ class ApplikasjonsTest: StringSpec({
         props[StreamsConfig.APPLICATION_ID_CONFIG] = "test"
         props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = "dummy:1234"
         props[StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
-        props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = SpecificAvroSerde::class.java.name
-        props["schema.registry.url"] = "http://dummy:1234"
+        props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = GenericSerde::class.java.name
+        props["auto.register.schemas"] = "true"
+        props["schema.registry.url"] = "mock://$SCHEMA_REGISTRY_SCOPE"
+
         val testDriver = TopologyTestDriver(topology, props)
         val eventlog = testDriver.createInputTopic(
             "eventlogTopic",
             Serdes.String().serializer(),
-            SpecificAvroSerde<Hendelse>().serializer()
+            hendelseSerde.serializer()
         )
         val ut = testDriver.createOutputTopic(
             "periodeTopic",
             Serdes.String().deserializer(),
-            SpecificAvroSerde<PeriodeTilstandV1>().deserializer()
+            periodeSerde.deserializer()
         )
         val start = Hendelse(
             UUID.randomUUID(),
