a20d9289ffcd3ac9e1e20692476218ab2a370b53
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-10-11 10:40:26 +0200

    Legger til rute for sjekk om kan-registreres-som-arbeidssoker

--- src/main/kotlin/no/nav/paw/arbeidssokerregisteret/services/ArbeidssokerService.kt
+++ src/main/kotlin/no/nav/paw/arbeidssokerregisteret/services/ArbeidssokerService.kt
@@ -1,10 +1,12 @@
 package no.nav.paw.arbeidssokerregisteret.services
 
+import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.runBlocking
 import no.nav.paw.arbeidssokerregisteret.domain.Foedselsnummer
 import no.nav.paw.arbeidssokerregisteret.domain.harOppholdstillatelse
 import no.nav.paw.arbeidssokerregisteret.intern.StartV1
 import no.nav.paw.arbeidssokerregisteret.kafka.producers.ArbeidssokerperiodeStartProducer
+import no.nav.paw.arbeidssokerregisteret.plugins.StatusException
 import no.nav.paw.arbeidssokerregisteret.utils.logger
 import no.nav.paw.pdl.PdlClient
 import no.nav.paw.pdl.hentOpphold
@@ -21,11 +23,30 @@ class ArbeidssokerService(
         }
             .harOppholdstillatelse()
 
-        logger.info("Oppholdsstatus: $harOpphold")
+        if (!harOpphold) {
+            logger.info("Bruker har ikke oppholdstillatelse, starter ikke arbeidssokerperiode")
+            throw StatusException(HttpStatusCode.Forbidden, "Bruker har ikke oppholdstillatelse")
+        }
+
         arbeidssokerperiodeStartProducer.publish(StartV1(foedselsnummer.verdi, Instant.now(), opprettetAv, "kilde"))
     }
 
     fun avsluttArbeidssokerperiode(foedselsnummer: Foedselsnummer, opprettetAv: String) {
         TODO()
+    }
+
+    fun kanRegistreresSomArbeidssoker(foedselsnummer: Foedselsnummer): Boolean {
+        val harOpphold = runBlocking {
+            pdlClient.hentOpphold(foedselsnummer.verdi, MDC.get("x_callId"), "paw-arbeidssokerregisteret")
+        }
+            .harOppholdstillatelse()
+
+        if (!harOpphold) {
+            logger.info("Bruker har ikke oppholdstillatelse, starter ikke arbeidssokerperiode")
+            // TODO: Mer informasjon om hvorfor bruker ikke kan registrere seg som arbeidssøker
+            throw StatusException(HttpStatusCode.Forbidden, "Bruker har ikke oppholdstillatelse")
+        }
+
+        return true
     }
 }
