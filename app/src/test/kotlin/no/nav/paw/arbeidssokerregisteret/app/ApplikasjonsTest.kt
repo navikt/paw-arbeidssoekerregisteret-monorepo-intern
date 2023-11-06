7e0a91012deb8fd09e37474e66e8d2dec8e32ab9
Nils Martin Sande <nils.martin.sande@nav.no>
2023-11-06 18:38:02 +0100

    Utvidet applikasjons test

--- app/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/app/ApplikasjonsTest.kt
+++ app/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/app/ApplikasjonsTest.kt
@@ -1,77 +1,121 @@
 package no.nav.paw.arbeidssokerregisteret.app
 
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
-import no.nav.paw.arbeidssokerregisteret.api.v1.Periode
-import no.nav.paw.arbeidssokerregisteret.api.v1.Situasjon
-import no.nav.paw.arbeidssokerregisteret.app.tilstand.TilstandSerde
-import no.nav.paw.arbeidssokerregisteret.intern.v1.Bruker
-import no.nav.paw.arbeidssokerregisteret.intern.v1.BrukerType
-import no.nav.paw.arbeidssokerregisteret.intern.v1.Metadata
-import no.nav.paw.arbeidssokerregisteret.intern.v1.Startet
-import org.apache.avro.specific.SpecificRecord
+import no.nav.paw.arbeidssokerregisteret.intern.v1.*
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.TopologyTestDriver
 import java.time.Instant
-import no.nav.paw.arbeidssokerregisteret.api.v1.Periode as ApiPeriode
+import java.util.*
 
 
-class ApplikasjonsTest : StringSpec({
-    "Verifiser at vi oppretter en ny periode ved førstegangs registrering" {
-        val hendelseSerde = opprettSerde<SpecificRecord>()
-        val periodeSerde = opprettSerde<Periode>()
-        val situasjonSerde = opprettSerde<Situasjon>()
-        val tilstandSerde = TilstandSerde()
-        val dbNavn = "tilstandsDb"
+class ApplikasjonsTest : FreeSpec({
+    val topology = topology(
+        builder = opprettStreamsBuilder(dbNavn, tilstandSerde),
+        dbNavn = dbNavn,
+        innTopic = eventlogTopicNavn,
+        periodeTopic = periodeTopicNavn,
+        situasjonTopic = situasjonTopicNavn
+    )
 
-        val eventlogTopicNavn = "eventlogTopic"
-        val periodeTopicNavn = "periodeTopic"
-        val situasjonTopicNavn = "situasjonTopic"
-        val topology = topology(
-            builder = opprettStreamsBuilder(dbNavn, tilstandSerde),
-            dbNavn = dbNavn,
-            innTopic = eventlogTopicNavn,
-            periodeTopic = periodeTopicNavn,
-            situasjonTopic = situasjonTopicNavn
-        )
-
-        val testDriver = TopologyTestDriver(topology, kafkaStreamProperties)
-        val eventlogTopic = testDriver.createInputTopic(
-            eventlogTopicNavn,
-            Serdes.Long().serializer(),
-            hendelseSerde.serializer()
-        )
-        val periodeTopic = testDriver.createOutputTopic(
-            periodeTopicNavn,
-            Serdes.Long().deserializer(),
-            periodeSerde.deserializer()
-        )
-        val situasjonTopic = testDriver.createOutputTopic(
-            situasjonTopicNavn,
-            Serdes.Long().deserializer(),
-            situasjonSerde.deserializer()
-        )
-        val start = Startet(
-            "12345678901",
+    val testDriver = TopologyTestDriver(topology, kafkaStreamProperties)
+    val eventlogTopic = testDriver.createInputTopic(
+        eventlogTopicNavn,
+        Serdes.Long().serializer(),
+        hendelseSerde.serializer()
+    )
+    val periodeTopic = testDriver.createOutputTopic(
+        periodeTopicNavn,
+        Serdes.Long().deserializer(),
+        periodeSerde.deserializer()
+    )
+    val situasjonTopic = testDriver.createOutputTopic(
+        situasjonTopicNavn,
+        Serdes.Long().deserializer(),
+        situasjonSerde.deserializer()
+    )
+    val identitetnummer = "12345678901"
+    val key = 5L
+    "Verifiser applikasjonsflyt" - {
+        val startet = Startet(
+            identitetnummer,
             Metadata(
                 Instant.now(),
                 Bruker(BrukerType.SYSTEM, "test"),
                 "unit-test",
-                "tester")
+                "tester"
+            )
         )
-        eventlogTopic.pipeInput(start.identitetsnummer.hashCode().toLong(), start)
-        val periode = periodeTopic.readKeyValue()
-        situasjonTopic.isEmpty shouldBe true
-        periode.key shouldBe start.identitetsnummer.hashCode().toLong()
-        periode.value.identitetsnummer shouldBe start.identitetsnummer
-        periode.value.startet.tidspunkt shouldBe start.metadata.tidspunkt
-        periode.value.startet.aarsak shouldBe start.metadata.aarsak
-        periode.value.startet.utfoertAv.type.name shouldBe start.metadata.utfoertAv.type.name
-        periode.value.startet.utfoertAv.id shouldBe start.metadata.utfoertAv.id
-        periode.value.avsluttet shouldBe null
-        periode.value.id.shouldNotBeNull()
+        var periodeId: UUID? = null
+        "Når vi mottar en 'startet' hendelse og ikke har noen tidligere tilstand skal vi opprette en ny periode" {
+            eventlogTopic.pipeInput(key, startet)
+            val periode = periodeTopic.readKeyValue()
+            situasjonTopic.isEmpty shouldBe true
+            verifiserPeriodeOppMotStartetOgStoppetHendelser(
+                forventetKafkaKey = key,
+                startet = startet,
+                stoppet = null,
+                mottattRecord = periode
+            )
+            periodeId = periode.value.id
+        }
 
+        "Når vi mottar en 'startet' hendelse for en person med en aktiv periode skal det ikke skje noe" {
+            val duplikatStart = Startet(
+                identitetnummer,
+                Metadata(
+                    Instant.now(),
+                    Bruker(BrukerType.SYSTEM, "test"),
+                    "unit-test",
+                    "tester"
+                )
+            )
+            eventlogTopic.pipeInput(key, duplikatStart)
+            periodeTopic.isEmpty shouldBe true
+            situasjonTopic.isEmpty shouldBe true
+        }
+
+        "Når vi mottar en ny situsjon for en person med en aktiv periode skal vi sende ut en ny situasjon" {
+            val situsjonMottat = SituasjonMottat(
+                identitetnummer,
+                Metadata(
+                    Instant.now(),
+                    Bruker(BrukerType.SYSTEM, "test"),
+                    "unit-test",
+                    "tester"
+                ),
+                Utdanning(Utdanningsnivaa.HOYERE_UTDANNING_1_TIL_4, JaNeiVetIkke.JA, JaNeiVetIkke.JA),
+                Helse(JaNeiVetIkke.JA),
+                Arbeidserfaring(JaNeiVetIkke.JA),
+                Arbeidsoekersituasjon(mutableListOf(Beskrivelse.ER_PERMITTERT))
+            )
+            eventlogTopic.pipeInput(key, situsjonMottat)
+            periodeTopic.isEmpty shouldBe true
+            val situasjon = situasjonTopic.readKeyValue()
+            situasjon.key shouldBe key
+            situasjon.value.periodeId shouldBe periodeId
+        }
+
+        "Når vi mottat en 'stoppet' hendelse for en person med en aktiv periode skal vi avslutte perioden" {
+            val stoppet = Stoppet(
+                identitetnummer,
+                Metadata(
+                    Instant.now(),
+                    Bruker(BrukerType.SYSTEM, "test"),
+                    "unit-test",
+                    "tester"
+                )
+            )
+            eventlogTopic.pipeInput(key, stoppet)
+            situasjonTopic.isEmpty shouldBe true
+            periodeTopic.isEmpty shouldBe false
+            val avsluttetPeriode = periodeTopic.readKeyValue()
+            verifiserPeriodeOppMotStartetOgStoppetHendelser(
+                forventetKafkaKey = key,
+                startet = startet,
+                stoppet = stoppet,
+                mottattRecord = avsluttetPeriode
+            )
+        }
     }
 })
-
--- app/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/app/test_utils.kt
+++ app/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/app/test_utils.kt
@@ -3,11 +3,18 @@ package no.nav.paw.arbeidssokerregisteret.app
 import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import no.nav.paw.arbeidssokerregisteret.api.v1.Periode
+import no.nav.paw.arbeidssokerregisteret.api.v1.Situasjon
 import no.nav.paw.arbeidssokerregisteret.app.tilstand.TilstandSerde
+import no.nav.paw.arbeidssokerregisteret.intern.v1.Startet
+import no.nav.paw.arbeidssokerregisteret.intern.v1.Stoppet
 import org.apache.avro.specific.SpecificRecord
 import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.common.utils.Time
+import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.state.internals.InMemoryKeyValueBytesStoreSupplier
@@ -15,6 +22,41 @@ import org.apache.kafka.streams.state.internals.KeyValueStoreBuilder
 import java.util.*
 
 const val SCHEMA_REGISTRY_SCOPE = "juni-registry"
+
+val hendelseSerde = opprettSerde<SpecificRecord>()
+val periodeSerde = opprettSerde<Periode>()
+val situasjonSerde = opprettSerde<Situasjon>()
+val tilstandSerde = TilstandSerde()
+const val dbNavn = "tilstandsDb"
+
+const val eventlogTopicNavn = "eventlogTopic"
+const val periodeTopicNavn = "periodeTopic"
+const val situasjonTopicNavn = "situasjonTopic"
+
+fun verifiserPeriodeOppMotStartetOgStoppetHendelser(
+    forventetKafkaKey: Long,
+    startet: Startet,
+    stoppet: Stoppet?,
+    mottattRecord: KeyValue<Long, Periode>
+) {
+    mottattRecord.key shouldBe forventetKafkaKey
+    mottattRecord.value.id.shouldNotBeNull()
+    mottattRecord.value.identitetsnummer shouldBe startet.identitetsnummer
+    mottattRecord.value.startet.tidspunkt shouldBe startet.metadata.tidspunkt
+    mottattRecord.value.startet.aarsak shouldBe startet.metadata.aarsak
+    mottattRecord.value.startet.utfoertAv.type.name shouldBe startet.metadata.utfoertAv.type.name
+    mottattRecord.value.startet.utfoertAv.id shouldBe startet.metadata.utfoertAv.id
+    if (stoppet == null) {
+        mottattRecord.value.avsluttet shouldBe null
+    } else {
+        val avsluttet = mottattRecord.value.avsluttet
+        avsluttet.shouldNotBeNull()
+        avsluttet.tidspunkt shouldBe stoppet.metadata.tidspunkt
+        avsluttet.aarsak shouldBe stoppet.metadata.aarsak
+        avsluttet.utfoertAv.type.name shouldBe stoppet.metadata.utfoertAv.type.name
+        avsluttet.utfoertAv.id shouldBe stoppet.metadata.utfoertAv.id
+    }
+}
 fun <T : SpecificRecord> opprettSerde(): Serde<T> {
     val schemaRegistryClient = MockSchemaRegistry.getClientForScope(SCHEMA_REGISTRY_SCOPE)
     val serde: Serde<T> = SpecificAvroSerde(schemaRegistryClient)
