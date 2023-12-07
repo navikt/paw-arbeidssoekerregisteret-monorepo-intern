34d20704195cde3e86a08228ec3424bb544acc8e
Nils Martin Sande <nils.martin.sande@nav.no>
2023-12-07 18:56:53 +0100

    La til håndtering av 'InvalidStateStoreException' i tilstands gauge

--- app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/app/metrics/StateGaugeTask.kt
+++ app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/app/metrics/StateGaugeTask.kt
@@ -3,6 +3,7 @@ package no.nav.paw.arbeidssokerregisteret.app.metrics
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import no.nav.paw.arbeidssokerregisteret.app.tilstand.Tilstand
 import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.errors.InvalidStateStoreException
 import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.util.concurrent.CompletableFuture
@@ -22,14 +23,19 @@ fun <T1> initStateGaugeTask(
         try {
             val gauge = StateGauge(registry)
             while (keepGoing.get()) {
-                val streamState = streamStateSupplier()
-                if (streamState == KafkaStreams.State.RUNNING) {
-                    val source = contentSupplier().flatMap(mapper)
-                    gauge.update(source)
-                    metricLogger.debug("Metrics oppdatert")
-                    Thread.sleep(Duration.ofMinutes(10))
-                } else {
-                    metricLogger.debug("KafkaStreamsState={}, metrics oppdateres ikke", streamState)
+                try {
+                    val streamState = streamStateSupplier()
+                    if (streamState == KafkaStreams.State.RUNNING) {
+                        val source = contentSupplier().flatMap(mapper)
+                        gauge.update(source)
+                        metricLogger.debug("Metrics oppdatert")
+                        Thread.sleep(Duration.ofMinutes(10))
+                    } else {
+                        metricLogger.debug("KafkaStreamsState={}, metrics oppdateres ikke", streamState)
+                        Thread.sleep(Duration.ofMinutes(1))
+                    }
+                } catch (ex: InvalidStateStoreException) {
+                    metricLogger.error("Metrics: state store ikke lenger gyldig", ex)
                     Thread.sleep(Duration.ofMinutes(1))
                 }
             }
