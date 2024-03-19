b7b3f71a44b735e11ed2e06d39889fac3d73dd3f
Nils Martin Sande <nils.martin.sande@nav.no>
2024-03-19 07:37:53 +0100

    Fikset m2m auth config

--- utgang-pdl/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utgang/pdl/clients/HttpClientsFactory.kt
+++ utgang-pdl/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utgang/pdl/clients/HttpClientsFactory.kt
@@ -1,7 +1,7 @@
 package no.nav.paw.arbeidssoekerregisteret.utgang.pdl.clients
 
 import no.nav.paw.config.hoplite.loadNaisOrLocalConfiguration
-import no.nav.paw.kafkakeygenerator.auth.AuthProvider
+import no.nav.paw.kafkakeygenerator.auth.AzureM2MConfig
 import no.nav.paw.kafkakeygenerator.auth.azureAdM2MTokenClient
 import no.nav.paw.kafkakeygenerator.auth.currentNaisEnv
 import no.nav.paw.kafkakeygenerator.client.KafkaKeyConfig
@@ -10,9 +10,9 @@ import no.nav.paw.kafkakeygenerator.client.kafkaKeysKlient
 
 fun createKafkaKeyGeneratorClient(): KafkaKeysClient {
     val naisEnv = currentNaisEnv
-    val authProvider = loadNaisOrLocalConfiguration<AuthProvider>("tokenx.toml")
+    val azureM2MConfig = loadNaisOrLocalConfiguration<AzureM2MConfig>("azure_m2m.toml")
     val kafkaKeyConfig = loadNaisOrLocalConfiguration<KafkaKeyConfig>("kafka_key_generator_client_config.toml")
-    val m2mTokenClient = azureAdM2MTokenClient(naisEnv, authProvider)
+    val m2mTokenClient = azureAdM2MTokenClient(naisEnv, azureM2MConfig)
     return kafkaKeysKlient(kafkaKeyConfig) {
         m2mTokenClient.createMachineToMachineToken(kafkaKeyConfig.scope)
     }
--- utgang-pdl/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utgang/pdl/clients/PdlClient.kt
+++ utgang-pdl/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utgang/pdl/clients/PdlClient.kt
@@ -1,7 +1,7 @@
 package no.nav.paw.arbeidssoekerregisteret.utgang.pdl.clients
 
 import no.nav.paw.config.hoplite.loadNaisOrLocalConfiguration
-import no.nav.paw.kafkakeygenerator.auth.AuthProvider
+import no.nav.paw.kafkakeygenerator.auth.AzureM2MConfig
 import no.nav.paw.kafkakeygenerator.auth.azureAdM2MTokenClient
 import no.nav.paw.kafkakeygenerator.auth.currentNaisEnv
 import no.nav.paw.pdl.PdlClient
@@ -10,8 +10,8 @@ import no.nav.paw.arbeidssoekerregisteret.utgang.pdl.config.PdlConfig
 
 fun createPdlClient(): PdlClient {
     val naisEnv = currentNaisEnv
-    val authProvider = loadNaisOrLocalConfiguration<AuthProvider>("tokenx.toml")
-    val m2mTokenClient = azureAdM2MTokenClient(naisEnv, authProvider)
+    val azureM2MConfig = loadNaisOrLocalConfiguration<AzureM2MConfig>("azure_m2m.toml")
+    val m2mTokenClient = azureAdM2MTokenClient(naisEnv, azureM2MConfig)
     val pdlConfig = loadNaisOrLocalConfiguration<PdlConfig>(PDL_CONFIG_FILE)
 
     return PdlClient(pdlConfig.url, pdlConfig.tema, createHttpClient()) {
