51d8403e633e30f09d513f6096a406ef80ca9a44
Nils Martin Sande <nils.martin.sande@nav.no>
2024-04-11 07:19:12 +0200

    Fikset avro schema metrics

--- app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/app/metrics/AvroSchemaGauge.kt
+++ app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/app/metrics/AvroSchemaGauge.kt
@@ -5,10 +5,12 @@ import no.nav.paw.arbeidssokerregisteret.app.helse.ModuleInfo
 import java.util.concurrent.atomic.AtomicLong
 
 private val buildTime = AtomicLong(0)
+private val majorVersion = AtomicLong(0)
 fun PrometheusMeterRegistry.registerAvroSchemaGauges(info: ModuleInfo) {
     buildTime.set(info.buildTime.toEpochMilli())
-    gauge(Names.AVRO_MAJOR_VERSION, info.version.split(".").first().toDouble())
-    gauge(Names.AVRO_SCHEMA_BUILD_TIME, buildTime)
+    majorVersion.set(info.version.split(".").first().toLong())
+    gauge(Names.AVRO_MAJOR_VERSION, majorVersion) { it.toDouble() }
+    gauge(Names.AVRO_SCHEMA_BUILD_TIME, buildTime) { it.toDouble() }
     gauge(Names.AVRO_SCHEMA_AGE, buildTime) { bt ->
         (System.currentTimeMillis() - bt.get()).toDouble()
     }
