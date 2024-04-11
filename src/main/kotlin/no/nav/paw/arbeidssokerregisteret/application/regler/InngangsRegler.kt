58edc335f4a617699687a13d27a0d9596e9dfe54
Nils Martin Sande <nils.martin.sande@nav.no>
2024-04-11 14:42:18 +0200

    Endret prioritering for regler

--- src/main/kotlin/no/nav/paw/arbeidssokerregisteret/application/regler/InngangsRegler.kt
+++ src/main/kotlin/no/nav/paw/arbeidssokerregisteret/application/regler/InngangsRegler.kt
@@ -3,18 +3,24 @@ package no.nav.paw.arbeidssokerregisteret.application.regler
 import no.nav.paw.arbeidssokerregisteret.application.*
 
 val reglerForInngangIPrioritertRekkefolge: List<Regel<EndeligResultat>> = listOf(
-    "Er forhåndsgodkjent av ansatt"(
-        Opplysning.FORHAANDSGODKJENT_AV_ANSATT,
-        id = RegelId.FORHAANDSGODKJENT_AV_ANSATT,
-        vedTreff = ::OK
-    ),
     "Person ikke funnet"(
         Opplysning.PERSON_IKKE_FUNNET,
         id = RegelId.IKKE_FUNNET,
         vedTreff = ::Avvist
     ),
-    "Er under 18 år"(
+    "Er registrert som død"(
+        Opplysning.DOED,
+        id = RegelId.DOED,
+        vedTreff = ::Avvist
+    ),
+    "Er forhåndsgodkjent av ansatt"(
+        Opplysning.FORHAANDSGODKJENT_AV_ANSATT,
+        id = RegelId.FORHAANDSGODKJENT_AV_ANSATT,
+        vedTreff = ::OK
+    ),
+    "Er bosatt i Norge i hendhold Folkeregisterloven, men er under 18 år"(
         Opplysning.ER_UNDER_18_AAR,
+        Opplysning.BOSATT_ETTER_FREG_LOVEN,
         id = RegelId.UNDER_18_AAR,
         vedTreff = ::Avvist
     ),
@@ -24,17 +30,12 @@ val reglerForInngangIPrioritertRekkefolge: List<Regel<EndeligResultat>> = listOf
         id = RegelId.UKJENT_ALDER,
         vedTreff = ::Avvist
     ),
-    "Er registrert som død"(
-        Opplysning.DOED,
-        id = RegelId.DOED,
-        vedTreff = ::Avvist
-    ),
     "Er registrert som savnet"(
         Opplysning.SAVNET,
         id = RegelId.SAVNET,
         vedTreff = ::Avvist
     ),
-    "Er over 18 år, er bosatt i Norge etter Folkeregisterloven"(
+    "Er over 18 år, er bosatt i Norge i hendhold Folkeregisterloven"(
         Opplysning.ER_OVER_18_AAR,
         Opplysning.BOSATT_ETTER_FREG_LOVEN,
         id = RegelId.OVER_18_AAR_OG_BOSATT_ETTER_FREG_LOVEN,
