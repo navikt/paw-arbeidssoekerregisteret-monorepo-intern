d8de9ffb70c97e09342ba80b9960f91b096cb872
robertkittilsen <robert.graarup.kittilsen@nav.no>
2024-04-15 16:04:58 +0200

    Legger til unikt name for counters for å skille på de

--- utgang-pdl/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utgang/pdl/metrics/Metrics.kt
+++ utgang-pdl/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utgang/pdl/metrics/Metrics.kt
@@ -6,17 +6,19 @@ import io.micrometer.prometheus.PrometheusMeterRegistry
 
 const val METRICS_UTGANG_PDL = "paw.arbeidssoekerregisteret.utgang.pdl"
 
-const val ANTALL_AVSLUTTET_HENDELSER = "antall_avsluttet_hendelser"
+const val AVSLUTTET_HENDELSE = ".avsluttet_hendelse"
 const val AVSLUTTET_HENDELSE_AARSAK = "avsluttet_hendelse_aarsak"
+
+const val PDL_HENT_PERSON = ".pdl_hent_person"
 const val PDL_HENT_PERSON_STATUS = "pdl_hent_person_status"
 
 fun PrometheusMeterRegistry.tellPdlAvsluttetHendelser(aarsak: String) = counter(
-    METRICS_UTGANG_PDL,
-    Tags.of(Tag.of(ANTALL_AVSLUTTET_HENDELSER, "avsluttet hendelse"), Tag.of(AVSLUTTET_HENDELSE_AARSAK, aarsak))
+    METRICS_UTGANG_PDL + AVSLUTTET_HENDELSE,
+    Tags.of(Tag.of(AVSLUTTET_HENDELSE_AARSAK, aarsak))
 ).increment()
 
 fun PrometheusMeterRegistry.tellStatusFraPdlHentPersonBolk(status: String) = counter(
-    METRICS_UTGANG_PDL,
+    METRICS_UTGANG_PDL + PDL_HENT_PERSON,
     Tags.of(Tag.of(PDL_HENT_PERSON_STATUS, status))
 ).increment()
 
