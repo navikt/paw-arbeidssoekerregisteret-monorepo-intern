cad0df44e53028d262b83a2b6ebf4c30e2ae8baa
Nils Martin Sande <nils.martin.sande@nav.no>
2024-04-23 12:03:14 +0200

    Fikset deprecation advarsel

--- app/src/main/kotlin/no/nav/paw/kafkakeygenerator/KafkaKeys.kt
+++ app/src/main/kotlin/no/nav/paw/kafkakeygenerator/KafkaKeys.kt
@@ -3,10 +3,9 @@ package no.nav.paw.kafkakeygenerator
 import no.nav.paw.kafkakeygenerator.database.IdentitetTabell
 import no.nav.paw.kafkakeygenerator.database.KafkaKeysTabell
 import no.nav.paw.kafkakeygenerator.vo.Identitetsnummer
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertIgnore
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class KafkaKeys(private val database: Database) {
@@ -14,11 +13,12 @@ class KafkaKeys(private val database: Database) {
     fun hent(identiteter: List<String>): Either<Failure, Map<String, Long>> =
         attempt {
             transaction(database) {
-                IdentitetTabell.select {
-                    IdentitetTabell.identitetsnummer inList identiteter
-                }.associate {
-                    it[IdentitetTabell.identitetsnummer] to it[IdentitetTabell.kafkaKey]
-                }
+                IdentitetTabell
+                    .selectAll()
+                    .where { IdentitetTabell.identitetsnummer inList identiteter }
+                    .associate {
+                        it[IdentitetTabell.identitetsnummer] to it[IdentitetTabell.kafkaKey]
+                    }
             }
         }.mapToFailure { exception ->
             Failure("database", FailureCode.INTERNAL_TECHINCAL_ERROR, exception)
@@ -27,9 +27,10 @@ class KafkaKeys(private val database: Database) {
     fun hent(identitet: Identitetsnummer): Either<Failure, Long> =
         attempt {
             transaction(database) {
-                IdentitetTabell.select {
-                    IdentitetTabell.identitetsnummer eq identitet.value
-                }.firstOrNull()?.get(IdentitetTabell.kafkaKey)
+                IdentitetTabell
+                    .selectAll()
+                    .where { IdentitetTabell.identitetsnummer eq identitet.value }
+                    .firstOrNull()?.get(IdentitetTabell.kafkaKey)
             }
         }.mapToFailure { exception ->
             Failure("database", FailureCode.INTERNAL_TECHINCAL_ERROR, exception)
