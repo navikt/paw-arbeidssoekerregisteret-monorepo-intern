e50bdd6d8f44f521b41a863cde46af204e5e3ab8
Nils Martin Sande <nils.martin.sande@nav.no>
2024-05-27 08:49:20 +0200

    Oppdaterte avhengigheter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ include(
 
 dependencyResolutionManagement {
     val githubPassword: String by settings
+    @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
         maven {
@@ -55,7 +56,7 @@ dependencyResolutionManagement {
 
         // Serialisering
         val orgApacheAvroVersion = "1.11.3"
-        val comFasterxmlJacksonVersion = "2.16.1"
+        val comFasterxmlJacksonVersion = "2.17.1"
 
         // ---- //
         val kotlinExposedVersion = "0.50.0"
@@ -69,16 +70,23 @@ dependencyResolutionManagement {
         val otelTargetSdkVersion = "1.36.0"
         val otelInstrumentationVersion = "2.1.0"
         val coroutinesVersion = "1.8.0"
+        val rapporteringsSchemaVersion = "24.05.15.2-1"
 
-        fun VersionCatalogBuilder.ktorLib(alias: String, artifactId: String) = library(alias, "io.ktor", artifactId).version("2.3.10")
-        fun VersionCatalogBuilder.ktorLibs(vararg aliases: Pair<String, String>) = aliases.forEach { (artifactId, alias) -> ktorLib(alias, artifactId) }
+        fun VersionCatalogBuilder.ktorLib(alias: String, artifactId: String) =
+            library(alias, "io.ktor", artifactId).version("2.3.10")
+
+        fun VersionCatalogBuilder.ktorLibs(vararg aliases: Pair<String, String>) =
+            aliases.forEach { (artifactId, alias) -> ktorLib(alias, artifactId) }
+
         infix fun String.alias(alias: String) = this to alias
         create("kotlinx") {
             library("coroutinesCore", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").version(coroutinesVersion)
         }
         create("loggingLibs") {
             library("logbackClassic", "ch.qos.logback", "logback-classic").version(logbackVersion)
-            library("logstashLogbackEncoder", "net.logstash.logback", "logstash-logback-encoder").version(logstashVersion)
+            library("logstashLogbackEncoder", "net.logstash.logback", "logstash-logback-encoder").version(
+                logstashVersion
+            )
         }
         create("ktorClient") {
             ktorLibs(
@@ -115,25 +123,40 @@ dependencyResolutionManagement {
             ktorLibs(
                 "ktor-serialization" alias "serialization",
                 "ktor-serialization-jvm" alias "serializationJvm",
-                "ktor-serialization-jackson" alias "serializationJackson"
+                "ktor-serialization-jackson" alias "serializationJackson",
+                "ktor-serialization-kotlinx-json" alias "serializationJson"
             )
         }
         create("otel") {
             library("api", "io.opentelemetry", "opentelemetry-api").version(otelTargetSdkVersion)
-            library("ktor","io.opentelemetry.instrumentation", "opentelemetry-ktor-2.0").version(otelInstrumentationVersion)
-            library("annotations", "io.opentelemetry.instrumentation", "opentelemetry-instrumentation-annotations").version(otelInstrumentationVersion)
+            library("ktor", "io.opentelemetry.instrumentation", "opentelemetry-ktor-2.0").version(
+                otelInstrumentationVersion
+            )
+            library(
+                "annotations",
+                "io.opentelemetry.instrumentation",
+                "opentelemetry-instrumentation-annotations"
+            ).version(otelInstrumentationVersion)
         }
         create("micrometer") {
             library("core", "io.micrometer", "micrometer-core").version(micrometerVersion)
-            library("registryPrometheus", "io.micrometer", "micrometer-registry-prometheus").version(micrometerVersion)
+            library("registryPrometheus", "io.micrometer", "micrometer-registry-prometheus").version(
+                micrometerVersion
+            )
         }
         create("pawClients") {
             library("pawPdlClient", "no.nav.paw", "pdl-client").version(pawPdlClientVersion)
             library("pawAaregClient", "no.nav.paw", "aareg-client").version(pawAaregClientVersion)
         }
         create("arbeidssoekerRegisteret") {
-            library("apiKotlin", "no.nav.paw.arbeidssokerregisteret.api.schema", "arbeidssoekerregisteret-kotlin").version(arbeidssokerregisteretVersion)
-            library("mainAvroSchema", "no.nav.paw.arbeidssokerregisteret.api", "main-avro-schema").version(arbeidssokerregisteretVersion)
+            library(
+                "apiKotlin",
+                "no.nav.paw.arbeidssokerregisteret.api.schema",
+                "arbeidssoekerregisteret-kotlin"
+            ).version(arbeidssokerregisteretVersion)
+            library("mainAvroSchema", "no.nav.paw.arbeidssokerregisteret.api", "main-avro-schema").version(
+                arbeidssokerregisteretVersion
+            )
         }
         create("orgApacheKafka") {
             library("kafkaClients", "org.apache.kafka", "kafka-clients").version(orgApacheKafkaVersion)
@@ -143,13 +166,17 @@ dependencyResolutionManagement {
         create("apacheAvro") {
             library("avro", "org.apache.avro", "avro").version(orgApacheAvroVersion)
             library("kafkaSerializer", "io.confluent", "kafka-avro-serializer").version(ioConfluentKafkaVersion)
-            library("kafkaStreamsAvroSerde", "io.confluent", "kafka-streams-avro-serde").version(ioConfluentKafkaVersion)
+            library("kafkaStreamsAvroSerde", "io.confluent", "kafka-streams-avro-serde").version(
+                ioConfluentKafkaVersion
+            )
         }
         create("jackson") {
-            library("datatypeJsr310", "com.fasterxml.jackson.datatype", "jackson-datatype-jsr310").version(comFasterxmlJacksonVersion)
-            library("kotlin", "com.fasterxml.jackson.module", "jackson-module-kotlin").version(comFasterxmlJacksonVersion)
-            library("ktorSerialization", "io.ktor", "ktor-serialization-jackson").withoutVersion()
-            library("serializationJvm", "io.ktor", "ktor-serialization-jackson-jvm").withoutVersion()
+            library("datatypeJsr310", "com.fasterxml.jackson.datatype", "jackson-datatype-jsr310").version(
+                comFasterxmlJacksonVersion
+            )
+            library("kotlin", "com.fasterxml.jackson.module", "jackson-module-kotlin").version(
+                comFasterxmlJacksonVersion
+            )
         }
         create("navCommon") {
             library("tokenClient", "no.nav.common", "token-client").version(noNavCommonVersion)
@@ -157,7 +184,9 @@ dependencyResolutionManagement {
             library("auditLog", "no.nav.common", "audit-log").version(noNavCommonVersion)
         }
         create("navSecurity") {
-            library("tokenValidationKtorV2", "no.nav.security", "token-validation-ktor-v2").version(noNavSecurityVersion)
+            library("tokenValidationKtorV2", "no.nav.security", "token-validation-ktor-v2").version(
+                noNavSecurityVersion
+            )
             library("tokenClient", "no.nav.security", "token-client-core").version(noNavSecurityVersion)
         }
         create("hoplite") {
@@ -173,6 +202,13 @@ dependencyResolutionManagement {
             library("javaTime", "org.jetbrains.exposed", "exposed-java-time").version(kotlinExposedVersion)
         }
         create("testLibs") {
+            bundle(
+                "withUnitTesting", listOf(
+                    "runnerJunit5",
+                    "assertionsCore",
+                    "mockk"
+                )
+            )
             library("runnerJunit5", "io.kotest", "kotest-runner-junit5").version(kotestVersion)
             library("assertionsCore", "io.kotest", "kotest-assertions-core").version(kotestVersion)
             library("mockk", "io.mockk", "mockk").version(mockkVersion)
@@ -182,6 +218,18 @@ dependencyResolutionManagement {
         }
         create("poao") {
             library("tilgangClient", "no.nav.poao-tilgang", "client").version("2024.04.29_13.59-a0ddddd36ac9")
+        }
+        create("rapportering") {
+            library(
+                "rapporteringsansvarSchema",
+                "no.nav.paw.arbeidssokerregisteret.api",
+                "rapporteringsansvar-schema"
+            ).version(rapporteringsSchemaVersion)
+            library(
+                "rapporteringsmeldingSchema",
+                "no.nav.paw.arbeidssokerregisteret.api",
+                "rapporteringsmelding-schema"
+            ).version(rapporteringsSchemaVersion)
         }
     }
 }
