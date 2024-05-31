4ded5350a348676486d7289b46ae436a07191526
Nils Martin Sande <nils.martin.sande@nav.no>
2024-05-31 09:06:42 +0200

    Fikset claim resolving (fungerte ikke med ny nav-sev pakke)

--- apps/api-start-stopp-perioder/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/utils/ResolvedClaims.kt
+++ apps/api-start-stopp-perioder/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/utils/ResolvedClaims.kt
@@ -6,16 +6,17 @@ class ResolvedClaims private constructor(
     private val map: Map<Claim<*>, Any>
 ) {
     constructor() : this(emptyMap())
+
     override fun toString(): String {
         return map.map { (key, value) -> "${key}=$value" }
             .let { "ResolvedClaims($it)" }
     }
 
     @Suppress("UNCHECKED_CAST")
-    operator fun <T: Any> get(claim: Claim<T>): T? = map[claim] as T?
+    operator fun <T : Any> get(claim: Claim<T>): T? = map[claim] as T?
     fun isResolved(claim: Claim<*>): Boolean = map.containsKey(claim)
 
-    fun <T: Any> add(claim: Claim<T>, rawValue: String): ResolvedClaims {
+    fun <T : Any> add(claim: Claim<T>, rawValue: String): ResolvedClaims {
         val parsed = claim.fromString(rawValue)
         val pair: Pair<Claim<T>, Any> = claim to parsed
         return ResolvedClaims(map + pair)
@@ -31,5 +32,10 @@ fun TokenValidationContext?.resolveClaims(vararg claims: Claim<*>): ResolvedClai
         }
 
 fun TokenValidationContext?.resolve(claim: Claim<*>): String? =
-    this?.getClaims(claim.issuer)
+    this?.getClaimOrNull(claim.issuer)
         ?.getStringClaim(claim.claimName)
+
+fun TokenValidationContext.getClaimOrNull(issuer: String) =
+    issuers
+        .firstOrNull { it.equals(issuer, ignoreCase = true) }
+        ?.let { presentIssuer -> getClaims(presentIssuer) }
