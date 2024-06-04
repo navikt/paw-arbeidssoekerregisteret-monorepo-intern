5b84fc72862a39cce338e152f8fd0f315308c432
robertkittilsen <robert.graarup.kittilsen@nav.no>
2024-06-04 16:23:39 +0200

    Korrigert swaggerFile for startstopp.

--- apps/api-start-stopp-perioder/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/routes/SwaggerRoutes.kt
+++ apps/api-start-stopp-perioder/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/routes/SwaggerRoutes.kt
@@ -4,6 +4,6 @@ import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.routing.Route
 
 fun Route.swaggerRoutes() {
-    swaggerUI(path = "docs/startstopp", swaggerFile = "openapi/opplysninger.yaml")
+    swaggerUI(path = "docs/startstopp", swaggerFile = "openapi/startstopp.yaml")
     swaggerUI(path = "docs/opplysninger", swaggerFile = "openapi/opplysninger.yaml")
 }
