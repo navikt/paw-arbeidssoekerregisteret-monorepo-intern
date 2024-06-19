954b82f17dec3addc917be441e2667ab47723325
Nils Martin Sande <nils.martin.sande@nav.no>
2024-06-19 11:23:42 +0200

    La til mock response for brukerstoette api

--- apps/hendelselogg-backup/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/backup/brukerstoette/mockService.kt
+++ apps/hendelselogg-backup/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/backup/brukerstoette/mockService.kt
@@ -1,9 +1,13 @@
 package no.nav.paw.arbeidssoekerregisteret.backup.brukerstoette
 
-import no.nav.paw.arbeidssoekerregisteret.backup.api.brukerstoette.models.DetaljerResponse
-import no.nav.paw.arbeidssoekerregisteret.backup.api.brukerstoette.models.Tilstand
-import no.nav.paw.arbeidssoekerregisteret.backup.api.brukerstoette.models.TilstandApiKall
+import no.nav.paw.arbeidssoekerregisteret.backup.api.brukerstoette.models.*
+import no.nav.paw.arbeidssokerregisteret.intern.v1.Avsluttet
+import no.nav.paw.arbeidssokerregisteret.intern.v1.Startet
+import no.nav.paw.arbeidssokerregisteret.intern.v1.vo.Bruker
+import no.nav.paw.arbeidssokerregisteret.intern.v1.vo.BrukerType
+import no.nav.paw.arbeidssokerregisteret.intern.v1.vo.Metadata
 import java.time.Instant
+import java.util.*
 
 fun getMockResponse(): DetaljerResponse {
     return DetaljerResponse(
@@ -20,6 +24,91 @@ fun getMockResponse(): DetaljerResponse {
                 harProfilering = true
             )
         ),
-        historikk = listOf()
+        historikk = listOf(
+            InnkommendeHendelse(
+                gjeldeneTilstand = Tilstand(
+                    harAktivePeriode = false,
+                    startet = Instant.now().minusSeconds(20000),
+                    avsluttet = Instant.now().minusSeconds(19000),
+                    harOpplysningerMottattHendelse = true,
+                    apiKall = TilstandApiKall(
+                        harOpplysning = true,
+                        harProfilering = true
+                    )
+                ),
+                nyTilstand = Tilstand(
+                    harAktivePeriode = true,
+                    startet = Instant.now().minusSeconds(10000),
+                    avsluttet = null,
+                    harOpplysningerMottattHendelse = true,
+                    apiKall = TilstandApiKall(
+                        harOpplysning = true,
+                        harProfilering = false
+                    )
+                ),
+                hendelse = Hendelse(
+                    hendelseId = UUID.randomUUID(),
+                    hendelseType = "Startet",
+                    timestamp = Instant.now().minusSeconds(19000),
+                    kafkaOffset = 2345423L,
+                    data = Startet(
+                        identitetsnummer = "12345678901",
+                        hendelseId = UUID.randomUUID(),
+                        id = 123L,
+                        metadata = Metadata(
+                            tidspunkt = Instant.now().minusSeconds(19000),
+                            utfoertAv = Bruker(
+                                id = "12345678901",
+                                type = BrukerType.SLUTTBRUKER
+                            ),
+                            kilde = "mock data",
+                            aarsak = "bare mock data"
+                        )
+                    )
+                )
+            ),
+            InnkommendeHendelse(
+                gjeldeneTilstand = Tilstand(
+                    harAktivePeriode = true,
+                    startet = Instant.now().minusSeconds(20000),
+                    avsluttet = Instant.now().minusSeconds(19000),
+                    harOpplysningerMottattHendelse = true,
+                    apiKall = TilstandApiKall(
+                        harOpplysning = true,
+                        harProfilering = true
+                    )
+                ),
+                nyTilstand = Tilstand(
+                    harAktivePeriode = false,
+                    startet = Instant.now().minusSeconds(10000),
+                    avsluttet = null,
+                    harOpplysningerMottattHendelse = true,
+                    apiKall = TilstandApiKall(
+                        harOpplysning = true,
+                        harProfilering = false
+                    )
+                ),
+                hendelse = Hendelse(
+                    hendelseId = UUID.randomUUID(),
+                    hendelseType = "Avsluttet",
+                    timestamp = Instant.now().minusSeconds(19000),
+                    kafkaOffset = 2345423L,
+                    data = Avsluttet(
+                        identitetsnummer = "12345678901",
+                        hendelseId = UUID.randomUUID(),
+                        id = 123L,
+                        metadata = Metadata(
+                            tidspunkt = Instant.now().minusSeconds(19000),
+                            utfoertAv = Bruker(
+                                id = "12345678901",
+                                type = BrukerType.SLUTTBRUKER
+                            ),
+                            kilde = "mock data",
+                            aarsak = "bare mock data"
+                        )
+                    )
+                )
+            )
+        )
     )
 }
--- apps/hendelselogg-backup/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/backup/initKtor.kt
+++ apps/hendelselogg-backup/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/backup/initKtor.kt
@@ -1,10 +1,15 @@
 package no.nav.paw.arbeidssoekerregisteret.backup
 
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import no.nav.paw.arbeidssoekerregisteret.backup.api.brukerstoette.models.DetaljerRequest
+import no.nav.paw.arbeidssoekerregisteret.backup.brukerstoette.getMockResponse
 import no.nav.paw.arbeidssoekerregisteret.backup.health.configureHealthRoutes
 import no.nav.paw.arbeidssoekerregisteret.backup.health.installMetrics
 import org.apache.kafka.clients.consumer.Consumer
@@ -18,6 +23,10 @@ fun initKtor(
         routing {
             swaggerUI(path = "docs/brukerstoette", swaggerFile = "openapi/Brukerstoette.yaml")
             configureHealthRoutes(prometheusMeterRegistry)
+            post("/api/v1/arbeidssoeker/detaljer") {
+                val request = call.receive<DetaljerRequest>()
+                call.respond(getMockResponse())
+            }
         }
     }.start(wait = false)
 }
