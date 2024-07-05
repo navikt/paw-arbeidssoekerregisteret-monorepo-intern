a0500bad1fa757819ed8b5db5cd8fa5adc2e9d7c
Nils Martin Sande <nils.martin.sande@nav.no>
2024-07-05 12:32:49 +0200

    La til CallLogging

--- apps/hendelselogg-backup/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/backup/initKtor.kt
+++ apps/hendelselogg-backup/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/backup/initKtor.kt
@@ -10,6 +10,7 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.request.*
@@ -40,6 +41,7 @@ fun initKtor(
 ) {
     embeddedServer(Netty, port = 8080) {
         configureHTTP(binders, prometheusMeterRegistry)
+        install(CallLogging)
         configureAuthentication(azureConfig)
         routing {
             swaggerUI(path = "docs/brukerstoette", swaggerFile = "openapi/Brukerstoette.yaml")
