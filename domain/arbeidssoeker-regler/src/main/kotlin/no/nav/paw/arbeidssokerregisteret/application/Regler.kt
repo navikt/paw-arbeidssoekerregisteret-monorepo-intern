63ff07322013f0c091b4573265c35ddab2a92f19
Nils Martin Sande <nils.martin.sande@nav.no>
2024-08-22 09:38:56 +0200

    La inn støtte for 'har ikke opplysning' i regler

--- domain/arbeidssoeker-regler/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/application/Regler.kt
+++ domain/arbeidssoeker-regler/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/application/Regler.kt
@@ -1,6 +1,15 @@
 package no.nav.paw.arbeidssokerregisteret.application
 
+import no.nav.paw.arbeidssokerregisteret.application.opplysninger.Opplysning
+
 interface Regler {
     val regler: List<Regel>
     val standardRegel: Regel
+    operator fun Opplysning.not(): Opplysning = HarIkke(this).value
 }
+
+interface Not<A> {
+    val value: A
+}
+
+data class HarIkke(override val value: Opplysning) : Not<Opplysning>
