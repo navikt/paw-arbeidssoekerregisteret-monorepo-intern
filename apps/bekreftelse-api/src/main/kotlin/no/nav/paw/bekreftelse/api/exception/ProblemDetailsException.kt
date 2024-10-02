53b45d061d8cc7f85cce1657a2f3219a1fcd805e
Thomas Johansen <thomas.johansen3@nav.no>
2024-10-02 13:13:10 +0200

    Fikset feil mapping av problem details

--- apps/bekreftelse-api/src/main/kotlin/no/nav/paw/bekreftelse/api/exception/ProblemDetailsException.kt
+++ apps/bekreftelse-api/src/main/kotlin/no/nav/paw/bekreftelse/api/exception/ProblemDetailsException.kt
@@ -3,6 +3,6 @@ package no.nav.paw.bekreftelse.api.exception
 import no.nav.paw.error.exception.ClientResponseException
 import no.nav.paw.error.model.ProblemDetails
 
-class ProblemDetailsException(val problem: ProblemDetails) : ClientResponseException(
-    problem.status, problem.code, problem.title, null
+class ProblemDetailsException(problem: ProblemDetails) : ClientResponseException(
+    problem.status, problem.code, problem.detail, null
 )
