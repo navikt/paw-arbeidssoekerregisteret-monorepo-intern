3ac5e0da1763f3b61ad855ee518f62a8b4b12043
Nils Martin Sande <nils.martin.sande@nav.no>
2024-11-11 14:24:40 +0100

    La til teste for id merge uten perioder

--- apps/kafka-key-maintenance/src/test/kotlin/no/nav/paw/kafkakeymaintenance/TestMergeMedEnAktivePeriode.kt
+++ apps/kafka-key-maintenance/src/test/kotlin/no/nav/paw/kafkakeymaintenance/TestMergeMedEnAktivePeriode.kt
@@ -4,34 +4,17 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
-import no.nav.paw.arbeidssokerregisteret.intern.v1.HarIdentitetsnummer
-import no.nav.paw.arbeidssokerregisteret.intern.v1.HendelseSerde
 import no.nav.paw.arbeidssokerregisteret.intern.v1.IdentitetsnummerSammenslaatt
-import no.nav.paw.kafkakeygenerator.client.Alias
-import no.nav.paw.kafkakeygenerator.client.LokaleAlias
-import no.nav.paw.kafkakeymaintenance.pdlprocessor.AktorTopologyConfig
-import no.nav.paw.kafkakeymaintenance.perioder.PeriodeRad
-import no.nav.paw.kafkakeymaintenance.perioder.statiskePerioder
-import no.nav.paw.test.kafkaStreamProperties
 import no.nav.paw.test.minutes
-import no.nav.paw.test.opprettSerde
-import no.nav.person.pdl.aktor.v2.Aktor
-import no.nav.person.pdl.aktor.v2.Identifikator
-import no.nav.person.pdl.aktor.v2.Type
-import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.KeyValue
-import org.apache.kafka.streams.TopologyTestDriver
-import org.apache.kafka.streams.state.Stores
-import java.time.Duration
 import java.time.Instant.parse
-import java.util.*
 
-class TopologyTest : FreeSpec({
+class TestMergeMedEnAktivePeriode : FreeSpec({
     val testTime = parse("2021-12-12T12:00:00Z")
     with(initTopologyTestContext(testTime)) {
-        "Test standard merge" {
+        "Test merge med aktive periode på den ene arbeidssøker iden" {
             val person1 = "12345678901"
             val person2 = "12345678902"
+            val person3 = "12345678903"
             addAlias(person1, alias(person1, 0L))
             addAlias(person2, alias(person2, 1L))
             addPeriode(testPeriode(identitetsnummer = person1, fra = "2021-12-04T12:00:00Z"))
@@ -40,6 +23,7 @@ class TopologyTest : FreeSpec({
             aktorTopic.pipeInput(
                 "p1",
                 aktor(//person1 og person2 er samme person i følge PDL, gjeldene identitetsnummer er person1
+                    id(identifikasjonsnummer = person3, gjeldende = false),
                     id(identifikasjonsnummer = person1, gjeldende = true),
                     id(identifikasjonsnummer = person2, gjeldende = false)
                 ),
@@ -64,6 +48,3 @@ class TopologyTest : FreeSpec({
 
     }
 })
-
-
-
