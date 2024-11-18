cc9c567507b81aae10a93be0441b802aeb60e43d
Nils Martin Sande <nils.martin.sande@nav.no>
2024-11-18 09:24:20 +0100

    Logg warning ved tom/null key

--- apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/pdlprocessor/lagring/LagePdlMeldinger.kt
+++ apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/pdlprocessor/lagring/LagePdlMeldinger.kt
@@ -4,17 +4,25 @@ import no.nav.paw.kafkakeymaintenance.kafka.TransactionContext
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import java.time.Instant
+
 private val lagreAktorLogger = LoggerFactory.getLogger("lagreAktorMelding")
 val lagreAktorMelding: TransactionContext.(ConsumerRecord<String, ByteArray>) -> Unit = { record ->
-    if (record.value() == null || record.value().isEmpty()) {
-        lagreAktorLogger.info("Sletter aktør: null={}, empty={}", record.value() == null, record.value()?.isEmpty())
-        delete(record.key())
-    } else {
-        insertOrUpdate(
-            record.key(),
-            timestamp = Instant.ofEpochMilli(record.timestamp()),
-            traceparant = record.headers().lastHeader("traceparent")?.value(),
-            data = record.value()
-        )
+    when {
+        record.value() == null || record.value().isEmpty() -> {
+            lagreAktorLogger.info("Sletter aktør: null={}, empty={}", record.value() == null, record.value()?.isEmpty())
+            delete(record.key())
+        }
+
+        else -> {
+            if (record.key().isNullOrBlank()) {
+                lagreAktorLogger.warn("Mottok melding med tom eller null key")
+            }
+            insertOrUpdate(
+                record.key(),
+                timestamp = Instant.ofEpochMilli(record.timestamp()),
+                traceparant = record.headers().lastHeader("traceparent")?.value(),
+                data = record.value()
+            )
+        }
     }
 }
