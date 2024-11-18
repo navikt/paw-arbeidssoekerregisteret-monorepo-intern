4c3d2031204e412888fd779a77d1fc9626c5ca10
Nils Martin Sande <nils.martin.sande@nav.no>
2024-11-18 10:27:19 +0100

    La in Avro deserializer for aktor record.key

--- apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/InitAktorConsumer.kt
+++ apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/InitAktorConsumer.kt
@@ -1,28 +1,37 @@
 package no.nav.paw.kafkakeymaintenance
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import no.nav.paw.config.kafka.KafkaFactory
+import no.nav.paw.config.kafka.plus
 import no.nav.paw.health.repository.HealthIndicatorRepository
 import no.nav.paw.kafkakeymaintenance.kafka.*
 import no.nav.paw.kafkakeymaintenance.pdlprocessor.lagring.lagreAktorMelding
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Duration
+import kotlin.reflect.KClass
 
 fun KafkaFactory.initAktorConsumer(
     healthIndicatorRepository: HealthIndicatorRepository,
     aktorTopic: Topic,
     applicationContext: ApplicationContext
 ): HwmConsumer<String, ByteArray> {
-
-    val aktorConsumer = createConsumer(
-        groupId = "kafka-key-maintenance-aktor-v${applicationContext.periodeConsumerVersion}",
-        clientId = "kafka-key-maintenance-aktor-client-v${applicationContext.periodeConsumerVersion}",
-        keyDeserializer = StringDeserializer::class,
-        valueDeserializer = ByteArrayDeserializer::class,
-        autoCommit = false,
-        autoOffsetReset = "earliest",
-        maxPollrecords = 1000
+    val aktorConsumer: KafkaConsumer<String, ByteArray> = KafkaConsumer(
+        baseProperties +
+                mapOf(
+                    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to false,
+                    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+                    ConsumerConfig.GROUP_ID_CONFIG to "kafka-key-maintenance-aktor-v${applicationContext.periodeConsumerVersion}",
+                    ConsumerConfig.CLIENT_ID_CONFIG to "kafka-key-maintenance-aktor-client-v${applicationContext.periodeConsumerVersion}",
+                    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+                    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
+                    ConsumerConfig.MAX_POLL_RECORDS_CONFIG to 1000
+                )
     )
     val reblancingListener = HwmRebalanceListener(
         contextFactory = applicationContext.aktorTxContext,
