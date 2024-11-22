e01c77f8eef26ffc3dfda48dbce9a10b2c617f37
Nils Martin Sande <nils.martin.sande@nav.no>
2024-11-22 12:40:31 +0100

    Økte versjonsnummer på periode og aktor consumer (key maintenance)

--- apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/StaticConfig.kt
+++ apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/StaticConfig.kt
@@ -2,7 +2,7 @@ package no.nav.paw.kafkakeymaintenance
 
 import no.nav.paw.kafkakeymaintenance.kafka.Topic
 
-const val PERIODE_CONSUMER_GROUP_VERSION = 1
-const val AKTOR_CONSUMER_GROUP_VERSION = 4
+const val PERIODE_CONSUMER_GROUP_VERSION = 2
+const val AKTOR_CONSUMER_GROUP_VERSION = 5
 val PERIODE_TOPIC = Topic("paw.arbeidssokerperioder-v1")
 val AKTOR_TOPIC = Topic("pdl.aktor-v2")
