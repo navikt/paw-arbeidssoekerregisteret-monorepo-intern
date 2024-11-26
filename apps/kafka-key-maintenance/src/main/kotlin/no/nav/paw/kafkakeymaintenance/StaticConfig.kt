e5a80c8ecc4675684a42544500b33662f5f0a39f
Nils Martin Sande <nils.martin.sande@nav.no>
2024-11-26 10:38:03 +0100

    Bumper aktor consumer versjon

--- apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/StaticConfig.kt
+++ apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/StaticConfig.kt
@@ -3,6 +3,6 @@ package no.nav.paw.kafkakeymaintenance
 import no.nav.paw.kafkakeymaintenance.kafka.Topic
 
 const val PERIODE_CONSUMER_GROUP_VERSION = 2
-const val AKTOR_CONSUMER_GROUP_VERSION = 6
+const val AKTOR_CONSUMER_GROUP_VERSION = 7
 val PERIODE_TOPIC = Topic("paw.arbeidssokerperioder-v1")
 val AKTOR_TOPIC = Topic("pdl.aktor-v2")
