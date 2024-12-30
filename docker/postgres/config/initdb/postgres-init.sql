51b1ed0971c132fabb693477efcf111e250c0a8b
robertkittilsen <robert.graarup.kittilsen@nav.no>
2024-12-30 12:58:25 +0100

    Fjerner overflødig docker oppsett for kafka-key-maintenance

--- docker/postgres/config/initdb/postgres-init.sql
+++ docker/postgres/config/initdb/postgres-init.sql
@@ -1,5 +1,7 @@
 create user pawkafkakeys with password '5up3r_53cr3t_p455w0rd';
 create user bekreftelse_api with password '5up3r_53cr3t_p455w0rd';
+create user pawkafkakeymaintenance with password '5up3r_53cr3t_p455w0rd';
 
 create database pawkafkakeys with owner pawkafkakeys;
 create database bekreftelser with owner bekreftelse_api;
+create database pawkafkakeymaintenance with owner pawkafkakeymaintenance;
