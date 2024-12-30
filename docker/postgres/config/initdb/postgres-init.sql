216ea66f53f5f484bba8a84e31a849b876778a39
robertkittilsen <robert.graarup.kittilsen@nav.no>
2024-12-30 13:07:02 +0100

    Fjerner overflødig docker oppsett for hendelselogg-backup

--- docker/postgres/config/initdb/postgres-init.sql
+++ docker/postgres/config/initdb/postgres-init.sql
@@ -1,7 +1,9 @@
 create user pawkafkakeys with password '5up3r_53cr3t_p455w0rd';
 create user bekreftelse_api with password '5up3r_53cr3t_p455w0rd';
 create user pawkafkakeymaintenance with password '5up3r_53cr3t_p455w0rd';
+create user hendelselogg_backup with password '5up3r_53cr3t_p455w0rd';
 
 create database pawkafkakeys with owner pawkafkakeys;
 create database bekreftelser with owner bekreftelse_api;
 create database pawkafkakeymaintenance with owner pawkafkakeymaintenance;
+create database hendelselogg_backup with owner hendelselogg_backup;
