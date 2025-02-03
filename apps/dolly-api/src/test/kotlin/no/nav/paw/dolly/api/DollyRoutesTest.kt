e890479a92739a6c33aa5fc03446488da2b685ee
robertkittilsen <robert.graarup.kittilsen@nav.no>
2025-02-03 21:34:02 +0100

    WIP: fikser example i documentation.yaml

--- apps/dolly-api/src/test/kotlin/no/nav/paw/dolly/api/DollyRoutesTest.kt
+++ apps/dolly-api/src/test/kotlin/no/nav/paw/dolly/api/DollyRoutesTest.kt
@@ -26,6 +26,7 @@ class DollyRoutesTest : FreeSpec({
         afterSpec {
             confirmVerified(
                 kafkaKeysClientMock,
+                oppslagClientMock,
                 hendelseKafkaProducerMock,
                 kafkaProducerMock,
                 dollyServiceMock
@@ -35,81 +36,84 @@ class DollyRoutesTest : FreeSpec({
 
         // TODO: test resten av api og legg til oppslag mock
 
-        "202 Accepted ved gyldig request" {
-            coEvery { kafkaKeysClientMock.getIdAndKey(any<String>()) } returns KafkaKeysResponse(
-                1,
-                1234
-            )
-            coEvery { hendelseKafkaProducerMock.sendHendelse(any(), any()) } returns Unit
-            testApplication {
-                configureTestApplication(dollyService)
+        "/api/v1/arbeidssoekerregistrering" - {
+            "202 Accepted ved gyldig request" {
+                coEvery { kafkaKeysClientMock.getIdAndKey(any<String>()) } returns KafkaKeysResponse(
+                    1,
+                    1234
+                )
+                coEvery { hendelseKafkaProducerMock.sendHendelse(any(), any()) } returns Unit
+                testApplication {
+                    configureTestApplication(dollyService)
 
-                val client = configureTestClient()
-                val arbeidssoekerregistreringRequest = TestData.nyArbeidssoekerregistreringRequest()
-                val response = client.post("/api/v1/arbeidssoekerregistrering") {
-                    bearerAuth(mockOAuth2Server.issueAzureM2MToken())
-                    setJsonBody(arbeidssoekerregistreringRequest)
+                    val client = configureTestClient()
+                    val arbeidssoekerregistreringRequest = TestData.nyArbeidssoekerregistreringRequest()
+                    val response = client.post("/api/v1/arbeidssoekerregistrering") {
+                        bearerAuth(mockOAuth2Server.issueAzureM2MToken())
+                        setJsonBody(arbeidssoekerregistreringRequest)
+                    }
+
+                    response.status shouldBe HttpStatusCode.Accepted
                 }
 
-                response.status shouldBe HttpStatusCode.Accepted
+                coVerify { kafkaKeysClientMock.getIdAndKey(any<String>()) }
+                coVerify { hendelseKafkaProducerMock.sendHendelse(any(), any()) }
             }
 
-            coVerify { kafkaKeysClientMock.getIdAndKey(any<String>()) }
-            coVerify { hendelseKafkaProducerMock.sendHendelse(any(), any()) }
-        }
+            "202 Accepted ved fullstendig request" {
+                coEvery { kafkaKeysClientMock.getIdAndKey(any<String>()) } returns KafkaKeysResponse(
+                    2,
+                    1235
+                )
+                coEvery { hendelseKafkaProducerMock.sendHendelse(any(), any()) } returns Unit
+                testApplication {
+                    configureTestApplication(dollyService)
 
-        "202 Accepted ved fullstendig request" {
-            coEvery { kafkaKeysClientMock.getIdAndKey(any<String>()) } returns KafkaKeysResponse(
-                2,
-                1235
-            )
-            coEvery { hendelseKafkaProducerMock.sendHendelse(any(), any()) } returns Unit
-            testApplication {
-                configureTestApplication(dollyService)
+                    val client = configureTestClient()
+                    val arbeidssoekerregistreringRequest = TestData.fullstendingArbeidssoekerregistreringRequest()
+                    val response = client.post("/api/v1/arbeidssoekerregistrering") {
+                        bearerAuth(mockOAuth2Server.issueAzureM2MToken())
+                        setJsonBody(arbeidssoekerregistreringRequest)
+                    }
 
-                val client = configureTestClient()
-                val arbeidssoekerregistreringRequest = TestData.fullstendingArbeidssoekerregistreringRequest()
-                val response = client.post("/api/v1/arbeidssoekerregistrering") {
-                    bearerAuth(mockOAuth2Server.issueAzureM2MToken())
-                    setJsonBody(arbeidssoekerregistreringRequest)
+                    response.status shouldBe HttpStatusCode.Accepted
                 }
 
-                response.status shouldBe HttpStatusCode.Accepted
+                coVerify { kafkaKeysClientMock.getIdAndKey(any<String>()) }
+                coVerify { hendelseKafkaProducerMock.sendHendelse(any(), any()) }
             }
 
-            coVerify { kafkaKeysClientMock.getIdAndKey(any<String>()) }
-            coVerify { hendelseKafkaProducerMock.sendHendelse(any(), any()) }
-        }
+            "400 Bad Request ved ugyldig request" {
+                testApplication {
+                    configureTestApplication(dollyService)
 
-        "400 Bad Request ved ugyldig request" {
-            testApplication {
-                configureTestApplication(dollyService)
+                    val client = configureTestClient()
+                    val response = client.post("/api/v1/arbeidssoekerregistrering") {
+                        bearerAuth(mockOAuth2Server.issueAzureM2MToken())
+                        setJsonBody("ugyldig request")
+                    }
 
-                val client = configureTestClient()
-                val response = client.post("/api/v1/arbeidssoekerregistrering") {
-                    bearerAuth(mockOAuth2Server.issueAzureM2MToken())
-                    setJsonBody("ugyldig request")
+                    response.status shouldBe HttpStatusCode.BadRequest
                 }
 
-                response.status shouldBe HttpStatusCode.BadRequest
+                confirmVerified(kafkaKeysClientMock, hendelseKafkaProducerMock)
             }
 
-            confirmVerified(kafkaKeysClientMock, hendelseKafkaProducerMock)
-        }
+            "403 Forbidden ved manglende token" {
+                testApplication {
+                    configureTestApplication(dollyService)
 
-        "403 Forbidden ved manglende token" {
-            testApplication {
-                configureTestApplication(dollyService)
+                    val client = configureTestClient()
+                    val response = client.post("/api/v1/arbeidssoekerregistrering") {
+                        setJsonBody(TestData.nyArbeidssoekerregistreringRequest())
+                    }
 
-                val client = configureTestClient()
-                val response = client.post("/api/v1/arbeidssoekerregistrering") {
-                    setJsonBody(TestData.nyArbeidssoekerregistreringRequest())
+                    response.status shouldBe HttpStatusCode.Forbidden
                 }
 
-                response.status shouldBe HttpStatusCode.Forbidden
+                confirmVerified(kafkaKeysClientMock, hendelseKafkaProducerMock)
             }
-
-            confirmVerified(kafkaKeysClientMock, hendelseKafkaProducerMock)
         }
+
     }
 })
