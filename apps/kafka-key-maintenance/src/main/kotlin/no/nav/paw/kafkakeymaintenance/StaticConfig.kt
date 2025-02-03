f7cb994d09d2140f5e748a8e5566978cba845ee5
Nils Martin Sande <nils.martin.sande@nav.no>
2025-02-03 07:11:58 +0100

    Deployer ny key-maintenance til prod

--- apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/StaticConfig.kt
+++ apps/kafka-key-maintenance/src/main/kotlin/no/nav/paw/kafkakeymaintenance/StaticConfig.kt
@@ -7,4 +7,4 @@ const val PERIODE_CONSUMER_GROUP_VERSION = 2
 const val AKTOR_CONSUMER_GROUP_VERSION = 11
 val PERIODE_TOPIC = Topic("paw.arbeidssokerperioder-v1")
 val AKTOR_TOPIC = Topic("pdl.aktor-v2")
-val START_DATO_FOR_MERGE_PROSESSERING = Instant.parse("2025-01-24T05:00:00Z")
+val START_DATO_FOR_MERGE_PROSESSERING = Instant.parse("2025-02-02T05:00:00Z")
