88961725208d43b86f57e0d06ed96f478bb20f54
Thomas Johansen <thomas.johansen3@nav.no>
2025-03-03 15:06:14 +0100

    Koblet inn konsumering av bekreftelser

--- apps/bekreftelse-min-side-oppgaver/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/bekreftelse/minsideoppgaver/applogic/Topology.kt
+++ apps/bekreftelse-min-side-oppgaver/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/bekreftelse/minsideoppgaver/applogic/Topology.kt
@@ -64,7 +64,6 @@ fun StreamsBuilder.bekreftelseKafkaTopology(
                 }
             }
 
-        /*
         stream(bekreftelseHendelseTopic, Consumed.with(Serdes.Long(), BekreftelseHendelseSerde()))
             .mapWithContext("bekreftelse-hendelse-mottatt", STATE_STORE_NAME.value) { hendelse ->
                 varselService.mottaBekreftelseHendelse(hendelse)
@@ -93,8 +92,8 @@ fun StreamsBuilder.bekreftelseKafkaTopology(
                 melding.varselId.toString() to melding.value
             }
             .foreach { key, meldinger ->
-                logger.debug("Sender meldinger: key: {}, value: {}", key, meldinger)
-            }*/
+                logger.debug("Skal sende meldinger: key: {}, value: {}", key, meldinger)
+            }
         // TODO: Disablet midlertidig .to(tmsVarselTopic, Produced.with(Serdes.String(), Serdes.String()))
     }
     return this
