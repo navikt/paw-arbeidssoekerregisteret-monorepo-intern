afb260c9ed45f6b20a9389f082307dea2587f650
Nils Martin Sande <nils.martin.sande@nav.no>
2025-03-10 09:07:53 +0100

    Fikset manglende ACR

--- lib/security/src/main/kotlin/no/nav/paw/security/authentication/model/Token.kt
+++ lib/security/src/main/kotlin/no/nav/paw/security/authentication/model/Token.kt
@@ -2,9 +2,9 @@ package no.nav.paw.security.authentication.model
 
 sealed class Token(val issuer: Issuer, val claims: List<Claim<*>>)
 
-data object TokenXToken : Token(TokenX, listOf(PID))
+data object TokenXToken : Token(TokenX, listOf(PID, ACR))
 data object AzureAdToken : Token(AzureAd, listOf(OID, Name, NavIdent, Roles))
-data object IdPortenToken : Token(IdPorten, listOf(PID))
+data object IdPortenToken : Token(IdPorten, listOf(PID, ACR))
 data object MaskinPortenToken : Token(MaskinPorten, emptyList())
 
 val validTokens: List<Token> = listOf(TokenXToken, AzureAdToken, IdPortenToken, MaskinPortenToken)
