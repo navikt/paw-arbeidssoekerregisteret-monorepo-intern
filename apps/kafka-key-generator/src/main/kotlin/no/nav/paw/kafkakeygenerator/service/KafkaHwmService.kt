ccc8cdfe88bf5e843562aadd6d35ae67ea46cf9d
Thomas Johansen <thomas.johansen3@nav.no>
2025-05-02 11:14:07 +0200

    Mottak av PDL aktor-meldinger
    Utvidet handtering av aktor-hendelser med feilsituasjoner

---
+++ apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/service/KafkaHwmService.kt
@@ -0,0 +1,46 @@
+package no.nav.paw.kafkakeygenerator.service
+
+import no.nav.paw.kafkakeygenerator.config.KafkaConsumerConfig
+import no.nav.paw.kafkakeygenerator.model.Hwm
+import no.nav.paw.kafkakeygenerator.model.asHwm
+import no.nav.paw.kafkakeygenerator.repository.HwmRepository
+import no.nav.paw.logging.logger.buildNamedLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.Instant
+
+class KafkaHwmService(
+    private val kafkaConsumerConfig: KafkaConsumerConfig,
+    private val hwmRepository: HwmRepository
+) : KafkaHwmOperations {
+    private val logger = buildNamedLogger("application.kafka.hwm")
+
+    override fun initHwm(topic: String, partitionCount: Int): Int {
+        return transaction {
+            with(kafkaConsumerConfig) {
+                logger.info("Initializing HWM for {} partitions on topic {}", partitionCount, topic)
+                (0 until partitionCount)
+                    .filter { hwmRepository.getByTopicAndPartition(version, topic, it) == null }
+                    .sumOf { hwmRepository.insert(version, topic, it, -1) }
+            }
+        }
+    }
+
+    override fun getHwm(topic: String, partition: Int): Hwm {
+        return transaction {
+            with(kafkaConsumerConfig) {
+                logger.info("Getting HWM for partition {} on topic {}", partition, topic)
+                val hwmRow = hwmRepository.getByTopicAndPartition(version, topic, partition)
+                requireNotNull(hwmRow?.asHwm()) { "No HWM found for partition $partition on topic $topic, init not called?" }
+            }
+        }
+    }
+
+    override fun updateHwm(topic: String, partition: Int, offset: Long, timestamp: Instant): Int {
+        return transaction {
+            with(kafkaConsumerConfig) {
+                logger.info("Updating HWM for partition {} on topic {}", partition, topic)
+                hwmRepository.update(version, topic, partition, offset, timestamp, Instant.now())
+            }
+        }
+    }
+}
