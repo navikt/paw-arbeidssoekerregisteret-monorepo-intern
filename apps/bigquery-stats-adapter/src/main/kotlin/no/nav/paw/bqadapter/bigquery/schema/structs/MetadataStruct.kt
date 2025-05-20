c8aad0cb7619c99da1e415d5c76e8666932ca823
Nils Martin Sande <nils.martin.sande@nav.no>
2025-05-20 13:16:13 +0200

    Fikset timezone id generering

--- apps/bigquery-stats-adapter/src/main/kotlin/no/nav/paw/bqadapter/bigquery/schema/structs/MetadataStruct.kt
+++ apps/bigquery-stats-adapter/src/main/kotlin/no/nav/paw/bqadapter/bigquery/schema/structs/MetadataStruct.kt
@@ -3,18 +3,16 @@ package no.nav.paw.bqadapter.bigquery.schema.structs
 import com.google.cloud.bigquery.Field
 import com.google.cloud.bigquery.FieldList
 import com.google.cloud.bigquery.StandardSQLTypeName
-import java.text.DateFormat
 import java.time.Instant
-import java.time.LocalDate
 import java.time.LocalDate.ofInstant
-import java.time.ZoneOffset
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 private const val metadata_tidspunkt = "tidspunkt"
 private const val metadata_kilde = "kilde"
 private const val metadata_aarsak = "aarsak"
 private const val metadata_brukertype = "brukertype"
-private val localTimeZone = ZoneOffset.of("Europe/Oslo")
+private val localTimeZone = ZoneId.of("Europe/Oslo")
 private val dateTimeFormater = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
 val metadataStruct get(): FieldList = FieldList.of(
