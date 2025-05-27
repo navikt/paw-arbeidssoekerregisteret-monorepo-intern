c6fc591a11563c01778117dd75d8345d5e141882
Thomas Johansen <thomas.johansen3@nav.no>
2025-05-27 09:56:39 +0200

    Fullførte konflikt- og hendelsehåndtering

--- apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/config/ApplicationConfig.kt
+++ apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/config/ApplicationConfig.kt
@@ -1,19 +1,34 @@
 package no.nav.paw.kafkakeygenerator.config
 
+import java.time.Duration
+
 const val APPLICATION_CONFIG = "application_config.toml"
 
 data class ApplicationConfig(
     val pawHendelseConsumer: KafkaConsumerConfig,
     val pawPeriodeConsumer: KafkaConsumerConfig,
-    val pdlAktorConsumer: KafkaConsumerConfig
+    val pdlAktorConsumer: KafkaConsumerConfig,
+    val pawIdentitetProducer: KafkaProducerConfig,
+    val identitetKonfliktJob: ScheduledJobConfig,
+    val identitetHendelseJob: ScheduledJobConfig
 )
 
 data class KafkaConsumerConfig(
     val version: Int,
     val topic: String,
-    val defaultPartitionCount: Int,
     val groupIdPrefix: String,
 ) {
     val groupId: String get() = "$groupIdPrefix-v$version"
     val clientId: String get() = "$groupIdPrefix-v$version-consumer"
 }
+
+data class KafkaProducerConfig(
+    val topic: String,
+    val clientId: String
+)
+
+data class ScheduledJobConfig(
+    val enabled: Boolean,
+    val delay: Duration,
+    val interval: Duration,
+)
