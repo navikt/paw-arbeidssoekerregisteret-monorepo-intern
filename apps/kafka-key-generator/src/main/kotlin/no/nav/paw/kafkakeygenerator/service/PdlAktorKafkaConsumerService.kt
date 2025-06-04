d6f58b2f446ceac0247fc82d801dd6eda935e958
Thomas Johansen <thomas.johansen3@nav.no>
2025-06-04 07:16:57 +0200

    La til team logg for feil

--- apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/service/PdlAktorKafkaConsumerService.kt
+++ apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/service/PdlAktorKafkaConsumerService.kt
@@ -3,7 +3,8 @@ package no.nav.paw.kafkakeygenerator.service
 import io.opentelemetry.instrumentation.annotations.WithSpan
 import no.nav.paw.kafkakeygenerator.config.KafkaConsumerConfig
 import no.nav.paw.kafkakeygenerator.model.asIdentitet
-import no.nav.paw.kafkakeygenerator.repository.KafkaKeysIdentitetRepository
+import no.nav.paw.kafkakeygenerator.model.asPerson
+import no.nav.paw.kafkakeygenerator.utils.SecureLogger
 import no.nav.paw.logging.logger.buildNamedLogger
 import no.nav.person.pdl.aktor.v2.Aktor
 import org.apache.kafka.clients.consumer.ConsumerRecord
@@ -14,8 +15,7 @@ import java.time.Instant
 class PdlAktorKafkaConsumerService(
     private val kafkaConsumerConfig: KafkaConsumerConfig,
     private val hwmOperations: KafkaHwmOperations,
-    private val identitetService: IdentitetService,
-    private val kafkaKeysIdentitetRepository: KafkaKeysIdentitetRepository
+    private val identitetService: IdentitetService
 ) {
     private val logger = buildNamedLogger("application.kafka")
 
@@ -26,47 +26,64 @@ class PdlAktorKafkaConsumerService(
                 logger.trace("Ingen aktor-meldinger mottatt i poll-vindu fra {}", topic)
             } else {
                 logger.debug("Mottok {} aktor-meldinger fra {}", records.count(), topic)
-                records.forEach(::handleRecord)
+                transaction {
+                    records.forEach(::handleRecord)
+                }
             }
         }
     }
 
     @WithSpan
     private fun handleRecord(record: ConsumerRecord<Any, Aktor>) {
-        transaction {
-            with(kafkaConsumerConfig) {
-                try {
-                    val rowsAffected = hwmOperations.updateHwm(
-                        topic = topic,
-                        partition = record.partition(),
-                        offset = record.offset(),
-                        timestamp = Instant.ofEpochMilli(record.timestamp())
-                    )
+        with(kafkaConsumerConfig) {
+            try {
+                val rowsAffected = hwmOperations.updateHwm(
+                    topic = topic,
+                    partition = record.partition(),
+                    offset = record.offset(),
+                    timestamp = Instant.ofEpochMilli(record.timestamp())
+                )
 
-                    if (rowsAffected == 0) {
-                        logger.warn(
-                            "Ignorerer aktor-melding på grunn av at offset {} på partition {} fra topic {} ikke er over HWM",
-                            record.offset(),
+                if (rowsAffected == 0) {
+                    logger.warn(
+                        "Ignorerer aktor-melding på grunn av at offset {} på partition {} fra topic {} ikke er over HWM",
+                        record.offset(),
+                        record.partition(),
+                        topic
+                    )
+                } else {
+                    logger.info(
+                        "Håndterer aktor-melding med offset {} på partition {} fra topic {}",
+                        record.offset(),
+                        record.partition(),
+                        topic
+                    )
+                    /* TODO handleAktor(
+                        aktorId = record.key().toString(),
+                        aktor = record.value(),
+                        sourceTimestamp = Instant.ofEpochMilli(record.timestamp())
+                    )*/
+
+                    if (setOf("2002308243366", "2647237114816").contains(record.key())) {
+                        SecureLogger.warn(
+                            "Aktor partition: {} offset: {} timestamp: {} key: {} value: {}",
                             record.partition(),
-                            topic
-                        )
-                    } else {
-                        logger.info(
-                            "Håndterer aktor-melding med offset {} på partition {} fra topic {}",
                             record.offset(),
-                            record.partition(),
-                            topic
-                        )
-                        handleAktor(
-                            aktorId = record.key().toString(),
-                            aktor = record.value(),
-                            sourceTimestamp = Instant.ofEpochMilli(record.timestamp())
+                            Instant.ofEpochMilli(record.timestamp()),
+                            record.key(),
+                            record.value()?.asPerson() ?: "null"
                         )
                     }
-                } catch (e: Exception) {
-                    logger.error("Håndterer av aktor-melding feilet", e)
-                    throw e
                 }
+            } catch (e: Exception) {
+                logger.error("Håndterer av aktor-melding feilet", e)
+                SecureLogger.error(
+                    "Håndterer av aktor-melding feilet key: {} value: {} timestamp: {}",
+                    record.key(),
+                    record.value()?.asPerson() ?: "null",
+                    Instant.ofEpochMilli(record.timestamp()),
+                )
+                throw e
             }
         }
     }
