0bf1e2786c19590172058339eb7b2d4e6b31ae62
Nils Martin Sande <nils.martin.sande@nav.no>
2025-11-24 08:29:31 +0100

    Fikset manglende subscribe på bekreftelse og på-vegne-av

--- apps/bigquery-stats-adapter/src/main/kotlin/no/nav/paw/bqadapter/App.kt
+++ apps/bigquery-stats-adapter/src/main/kotlin/no/nav/paw/bqadapter/App.kt
@@ -49,7 +49,7 @@ fun main() {
     val kafkaConfig = loadNaisOrLocalConfiguration<KafkaConfig>(KAFKA_CONFIG_WITH_SCHEME_REG)
     val kafkaFactory = KafkaFactory(kafkaConfig)
     val consumer = kafkaFactory.createConsumer<Long, ByteArray>(
-        groupId = "bq-consumer-v7",
+        groupId = "bq-consumer-v8",
         clientId = "bq-consumer-v3-${System.currentTimeMillis()}",
         keyDeserializer = LongDeserializer::class,
         valueDeserializer = ByteArrayDeserializer::class,
@@ -80,7 +80,9 @@ fun main() {
             kafkaConsumerWrapper = CommittingKafkaConsumerWrapper(
                 topics = listOf(
                     bigqueryAppContext.topics.hendelseloggTopic,
-                    bigqueryAppContext.topics.periodeTopic
+                    bigqueryAppContext.topics.periodeTopic,
+                    bigqueryAppContext.topics.paavnegneavTopic,
+                    bigqueryAppContext.topics.bekreftelseTopic,
                 ),
                 consumer = consumer,
                 exceptionHandler = { throwable ->
