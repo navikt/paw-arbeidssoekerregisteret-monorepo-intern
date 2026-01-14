d535c8f1ad4b68832227960922c10f68f399e43f
Thomas Johansen <thomas.johansen3@nav.no>
2026-01-14 09:57:48 +0100

    Forbedret henting av konflikter

--- apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/service/IdentitetResponseService.kt
+++ apps/kafka-key-generator/src/main/kotlin/no/nav/paw/kafkakeygenerator/service/IdentitetResponseService.kt
@@ -25,6 +25,49 @@ class IdentitetResponseService(
         hentPdl: Boolean = false
     ): IdentitetResponse {
         logger.debug("Henter alle identiteter relatert til gitt identitet")
+        val identitetRows = IdentiteterTable
+            .findAllByIdentitet(identitet)
+            .filter { it.status != IdentitetStatus.SLETTET }
+
+        val konflikter = if (visKonflikter) {
+            val aktorIdListe = identitetRows
+                .map { it.aktorId }
+                .distinct()
+            val arbeidssoekerIdListe = identitetRows
+                .map { it.arbeidssoekerId }
+                .distinct()
+            val konflikter = KonflikterTable.findByIdentitetAndStatus(
+                identitet = identitet,
+                status = KonfliktStatus.VENTER
+            )
+            if (konflikter.isNotEmpty()) {
+                konflikter
+                    .map {
+                        Konflikt(
+                            type = it.type.asApi(),
+                            detaljer = KonfliktDetaljer(
+                                aktorIdListe = aktorIdListe,
+                                arbeidssoekerIdListe = arbeidssoekerIdListe
+                            )
+                        )
+                    }
+            } else if (aktorIdListe.size > 1 || arbeidssoekerIdListe.size > 1) {
+                listOf(
+                    Konflikt(
+                        type = KonfliktType.MERGE,
+                        detaljer = KonfliktDetaljer(
+                            aktorIdListe = aktorIdListe,
+                            arbeidssoekerIdListe = arbeidssoekerIdListe
+                        )
+                    )
+                )
+            } else {
+                emptyList()
+            }
+        } else {
+            null
+        }
+
         val pdlIdentiteter = if (hentPdl) {
             pdlService.finnIdentiteter(identitet = identitet)
                 .map { it.asIdentitet() }
@@ -32,54 +75,16 @@ class IdentitetResponseService(
         } else {
             null
         }
-        val identitetRows = IdentiteterTable
-            .findAllByIdentitet(identitet)
-            .filter { it.status != IdentitetStatus.SLETTET }
+
         if (identitetRows.isEmpty()) {
             return IdentitetResponse(
                 identiteter = emptyList(),
                 pdlIdentiteter = pdlIdentiteter,
+                konflikter = konflikter
             )
         } else {
             val arbeidssoekerId = identitetRows.maxOf { it.arbeidssoekerId }
-            val konflikter = if (visKonflikter) {
-                val aktorIdListe = identitetRows
-                    .map { it.aktorId }
-                    .distinct()
-                val arbeidssoekerIdListe = identitetRows
-                    .map { it.arbeidssoekerId }
-                    .distinct()
-                val konflikter = KonflikterTable.findByIdentitetAndStatus(
-                    identitet = identitet,
-                    status = KonfliktStatus.VENTER
-                )
-                if (konflikter.isNotEmpty()) {
-                    konflikter
-                        .map {
-                            Konflikt(
-                                type = it.type.asApi(),
-                                detaljer = KonfliktDetaljer(
-                                    aktorIdListe = aktorIdListe,
-                                    arbeidssoekerIdListe = arbeidssoekerIdListe
-                                )
-                            )
-                        }
-                } else if (aktorIdListe.size > 1 || arbeidssoekerIdListe.size > 1) {
-                    listOf(
-                        Konflikt(
-                            type = KonfliktType.MERGE,
-                            detaljer = KonfliktDetaljer(
-                                aktorIdListe = aktorIdListe,
-                                arbeidssoekerIdListe = arbeidssoekerIdListe
-                            )
-                        )
-                    )
-                } else {
-                    emptyList()
-                }
-            } else {
-                null
-            }
+
             val identiteter = identitetRows
                 .map { it.asIdentitet() }
                 .toMutableList()
