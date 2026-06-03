d422a19796e9651774ad827e6bdcbe20e1b44814
Nils Martin Sande <nils.martin.sande@nav.no>
2026-06-03 07:41:07 +0200

    La til enkel test for å verifisere at vi alltid kan håndtere dagens avsluttet hendelser

--- domain/interne-hendelser/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/intern/v1/HendelseSerdeTest.kt
+++ domain/interne-hendelser/src/test/kotlin/no/nav/paw/arbeidssokerregisteret/intern/v1/HendelseSerdeTest.kt
@@ -1,10 +1,12 @@
 package no.nav.paw.arbeidssokerregisteret.intern.v1
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import no.nav.paw.arbeidssokerregisteret.intern.v1.vo.Opplysning
+import java.util.UUID
 
 class HendelseSerdeTest: FreeSpec({
     val serde = HendelseSerde()
@@ -19,9 +21,42 @@ class HendelseSerdeTest: FreeSpec({
             resultat.opplysninger shouldContain Opplysning.UKJENT_OPPLYSNING
             resultat.opplysninger.size shouldBe 2
         }
+        "Verifiser at deserialisering av gamle hendelser fremdeles fungerer" - {
+            "Vi kan deserialisere en 'avsluttet' hendelse generert i dev 2. juni 2026" {
+                val resultat = serde.deserializer().deserializeFromString(avsluttet_hendelse_dev_02062026)
+                resultat.shouldBeInstanceOf<Avsluttet>()
+                resultat.oppgittAarsak shouldBe Aarsak.RegisterGracePeriodeUtloeptEtterEksternInnsamling
+                resultat.kalkulertAarsak shouldBe Aarsak.Udefinert
+                resultat.opplysninger.shouldBeEmpty()
+            }
+        }
     }
 })
 
+val avsluttet_hendelse_dev_02062026 = """
+{
+    "hendelseId" : "af52f6f9-ebd5-4cd3-eeb4-3aefdecd7b12",
+    "id" : 139682,
+    "identitetsnummer" : "45419942222",
+    "metadata" : {
+        "tidspunkt":1780358119.801662,
+        "utfoertAv":{
+        "type":"SYSTEM",
+        "id":"europe-north1-docker.pkg.dev/nais-management-233d/paw/paw-arbeidssoekerregisteret-bekreftelse-utgang:26.05.29.327-1",
+        "sikkerhetsnivaa": null
+    },
+    "kilde" : "bekreftelse_paavegneav_stopp:dagpenger",
+    "aarsak" : "[Bekreftelse:ytelse/støtte] Ikke levert innen fristen",
+    "tidspunktFraKilde" : null
+    },
+    "opplysninger":[
+    ],
+    "periodeId" : "12b794c9-2d77-484f-8117-589a0d413566",
+    "kalkulertAarsak" : "Udefinert",
+    "oppgittAarsak" : "RegisterGracePeriodeUtloeptEtterEksternInnsamling",
+    "hendelseType" : "intern.v1.avsluttet"
+}
+""".trimIndent()
 
 val avvistUtenOpplysninger: String = """
 {
