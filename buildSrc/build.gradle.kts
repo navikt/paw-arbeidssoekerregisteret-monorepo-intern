a416ee138a093bfde5d55152d658c9e13070117e
Marius Eriksen <marius.eriksen@nav.no>
2026-08-05 11:29:46 +0200

    Oppdater flere avhengigheter

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.google.cloud.tools.jib:com.google.cloud.tools.jib.gradle.plugin:3.5.3")
+    implementation("com.google.cloud.tools.jib:com.google.cloud.tools.jib.gradle.plugin:3.5.4")
 }
 
 repositories {
